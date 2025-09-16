e92af4c096626072498f07caf5168163a8b9d118
Julio C Enciso-Alva <enciso.alva.jc@gmail.com>
2025-09-16 11:44:38 -0500

    More examples
    Problem with double rotations

--- examples_code/test06_doubledouble.m
+++ examples_code/test06_doubledouble.m
@@ -86,6 +86,15 @@ CtrlPtsArray = {c2,[...
   [-1,0]'
   ], c1};
 
+% rotated square
+CtrlPtsArray = {...
+  LineToBezier([-1,1]',[0,2]'), ...
+  LineToBezier([0,2]',[2,0]'), ...
+  LineToBezier([2,0]',[0,-2]'), ...
+  LineToBezier([0,-2]',[-2,0]'), ...
+  LineToBezier([-2,0]',[-1,1]') ...
+  };
+
 %%
 % self-intersecting shcape
 xx = 1;
@@ -157,23 +166,6 @@ CtrlPtsArray = {...
   };
 
 %%
-% hexagon
-
-% Define the hexagon control points
-hexagonPts = [cos(-(0:5)*2*pi/6+pi/2); sin(-(0:5)*2*pi/6+pi/2)] * 2; 
-pt0 = (hexagonPts(:,1)+hexagonPts(:,6))/2;
-
-CtrlPtsArray = {...
-  LineToBezier(pt0, hexagonPts(:,1)),...
-  LineToBezier(hexagonPts(:,1), hexagonPts(:,2)),...
-  LineToBezier(hexagonPts(:,2), hexagonPts(:,3)),...
-  LineToBezier(hexagonPts(:,3), hexagonPts(:,4)),...
-  LineToBezier(hexagonPts(:,4), hexagonPts(:,5)),...
-  LineToBezier(hexagonPts(:,5), hexagonPts(:,6)),...
-  LineToBezier(hexagonPts(:,6), pt0),...
-  };
-
-%%
 %star with 5 spikes
 aang   = -(0:(2*pi/5):(2*pi));
 aang(end) = [];
@@ -214,7 +206,7 @@ for i = 1:size(CtrlPtsArray,2)
 scatter(CtrlPtsArray{i}(1,:), CtrlPtsArray{i}(2,:))
 end
 
-BezOG  = AllBezierEval(CtrlPtsArray, 0.01);
+BezOG  = AllBezierEval(CtrlPtsArray, 0.001);
 figure()
 hold on
 axis equal
@@ -232,8 +224,8 @@ MaxSpins = 100;
 % designer stuff
 MarkerAngle0 = 0;
 
-WheelBezRatio = 6+5/7;
-WheelMarkerRatio = 4/3;
+WheelBezRatio = 4+3/5;
+WheelMarkerRatio = 1;
 
 % specific to this example
 %WheelRadius  = (BezierPerimeter(CtrlPtsArray,0.00001)/(2*pi))/WheelBezRatio;
@@ -244,6 +236,7 @@ CornerRoundingRadius = sqrt(0.005*BezierArea(CtrlPtsArray, MaxDistDelta)/(pi));
 
 %% 
 % remove inner corners
+if false
 [CtrlPtsArray_rounded_flipped] = ...
   RemoveAllCorners( FlipBezierAll(CtrlPtsArray), CornerRoundingRadius, MaxDistDelta, false );
 CtrlPtsArray = FlipBezierAll(CtrlPtsArray_rounded_flipped);
@@ -255,31 +248,10 @@ grid on
 for i = 1:size(CtrlPtsArray,2)
 scatter(CtrlPtsArray{i}(1,:), CtrlPtsArray{i}(2,:))
 end
-
-%%
-% plot
-if false
-WheelRadius = (BezierPerimeter(CtrlPtsArray,0.00001)/(2*pi))/WheelBezRatio;
-MarkerRadius = WheelRadius*WheelMarkerRatio;
-
-[BezierPos, ...
-  WhCtrPos1, MarkerPos1, MarkerAngle1,...
-  WhCtrPos2, MarkerPos2, MarkerAngle2] = ...
-  SetupCurves_2pts( CtrlPtsArray, WheelRadius, MarkerRadius, MarkerAngle0, ...
-    MaxDistDelta, CloseTol, MaxSpins);
-
-% preview
-figure()
-fill(BezierPos(1,:),BezierPos(2,:), 'k', 'EdgeColor', 'none'); 
-hold on
-axis equal
-grid on
-plot(MarkerPos1(1,:),MarkerPos1(2,:),'yellow')
-%plot(MarkerPos2(1,:),MarkerPos2(2,:),'magenta')
 end
 
 %% 
-% remove corners
+% remove corners inside and outside
 
 WheelRadiusTol = 0.000001;
 
@@ -287,8 +259,10 @@ WheelRadius_old = Inf;
 WheelRadius_new = (BezierPerimeter(CtrlPtsArray,0.00001)/(2*pi))/WheelBezRatio;
 
 while abs( WheelRadius_new - WheelRadius_old ) > WheelRadiusTol
+  [CtrlPtsArray_tmp_inv] = ...
+    RemoveAllCorners( FlipBezierAll(CtrlPtsArray), WheelRadius_new, MaxDistDelta, false );
   [CtrlPtsArray_tmp] = ...
-    RemoveAllCorners( CtrlPtsArray, WheelRadius_new, MaxDistDelta, false );
+    RemoveAllCorners( FlipBezierAll(CtrlPtsArray_tmp_inv), WheelRadius_new, MaxDistDelta, false );
   %
   WheelRadius_old = WheelRadius_new;
   WheelRadius_new = (BezierPerimeter(CtrlPtsArray_tmp,0.00001)/(2*pi))/WheelBezRatio
@@ -341,14 +315,186 @@ BezOG = AllBezierEval(CtrlPtsArray, MaxDistDelta);
 fill(BezOG(1,:),BezOG(2,:), 'r', 'EdgeColor', 'none'); 
 
 end
+
+%%
+[BezierPos, ...
+  WhCtrPos1A, MarkerPos1A, MarkerAngle1A,...
+  WhCtrPos2A, MarkerPos2A, MarkerAngle2A] = ...
+  SetupCurves_2pts( CtrlPtsArray_tmp, WheelRadius, MarkerRadius, MarkerAngle0, ...
+    MaxDistDelta, CloseTol, MaxSpins);
+
+[~, ...
+  WhCtrPos2B, MarkerPos2B, MarkerAngle2B,...
+  WhCtrPos1B, MarkerPos1B, MarkerAngle1B] = ...
+  SetupCurves_2pts( FlipBezierAll(CtrlPtsArray_tmp), WheelRadius, MarkerRadius, MarkerAngle0+pi, ...
+    MaxDistDelta, CloseTol, MaxSpins);
+
+WhCtrPos1B    = flip( WhCtrPos1B, 2 );
+MarkerPos1B   = flip(MarkerPos1B, 2);
+MarkerAngle1B = flip(MarkerAngle1B, 2);
+WhCtrPos2B    = flip(WhCtrPos2B, 2);
+MarkerPos2B   = flip(MarkerPos2B, 2);
+MarkerAngle2B = flip(MarkerAngle2B, 2);
+
+%%
+% preview
+figure()
+fill(BezierPos(1,:),BezierPos(2,:), 'k', 'EdgeColor', 'none'); 
+hold on
+axis equal
+grid on
+plot(MarkerPos1A(1,:),MarkerPos1A(2,:),'yellow')
+plot(MarkerPos2A(1,:),MarkerPos2A(2,:),'magenta')
+plot(MarkerPos1B(1,:),MarkerPos1B(2,:),'yellow')
+plot(MarkerPos2B(1,:),MarkerPos2B(2,:),'magenta')
+
+%%
+% video parameters
+TotalTime = 60;
+AfterTime = 5;
+
+VidName = 'doubledouble250916';
+
 %%
 % video
 
 close all
-MakeVideo_2pts( WheelRadius, ...
-  BezOG, ...
-  WhCtrPos1, MarkerPos1, MarkerAngle1,...
-  WhCtrPos2, MarkerPos2, MarkerAngle2,...
-  MaxDistDelta,...
-  60, 5, 'test_250916_12' )
-%  60, 5, 'test_250914_02' )
+
+TimeFromBez1A = zeros(1,size(WhCtrPos1A,2));
+TimeFromBez1A(2:end) = cumsum( vecnorm( diff(WhCtrPos1A,1,2), 2, 1 ) );
+
+TimeFromBez2A = zeros(1,size(WhCtrPos2A,2));
+TimeFromBez2A(2:end) = cumsum( vecnorm( diff(WhCtrPos2A,1,2), 2, 1 ) );
+
+TimeFromBez1B = zeros(1,size(WhCtrPos1B,2));
+TimeFromBez1B(2:end) = cumsum( vecnorm( diff(WhCtrPos1B,1,2), 2, 1 ) );
+
+TimeFromBez2B = zeros(1,size(WhCtrPos2B,2));
+TimeFromBez2B(2:end) = cumsum( vecnorm( diff(WhCtrPos2B,1,2), 2, 1 ) );
+
+% duration of video
+TimeFromBez1A = TimeFromBez1A*((TotalTime-AfterTime)/TimeFromBez1A(end));
+TimeFromBez2A = TimeFromBez2A*((TotalTime-AfterTime)/TimeFromBez2A(end));
+
+TimeFromBez1B = TimeFromBez1B*((TotalTime-AfterTime)/TimeFromBez1B(end));
+TimeFromBez2B = TimeFromBez2B*((TotalTime-AfterTime)/TimeFromBez2B(end));
+
+% parameters
+fps = 30;
+%MaxTime = ceil(TimeFromMarker(end)*fps)/fps;
+MaxTime = ceil(TimeFromBez1A(end)*fps)/fps;
+nTimes = MaxTime*fps;
+
+% help
+%idxx = 1:size(TimeFromMarker,2);
+idxx1A = 1:size(TimeFromBez1A,2);
+idxx2A = 1:size(TimeFromBez2A,2);
+idxx1B = 1:size(TimeFromBez1B,2);
+idxx2B = 1:size(TimeFromBez2B,2);
+
+aang = 0:(2*pi/ ceil( 2*pi/(MaxDistDelta/WheelRadius) )):(2*pi);
+circ = WheelRadius*[cos(aang); sin(aang)];
+aux_angles = 0:(pi/3):(2*pi);
+
+% original figure
+f1 = figure('Visible','off','Name','Just the curve');
+hold on
+axis equal
+axis off
+xlim([min(MarkerPos1A(1,:)) max(MarkerPos1A(1,:))])
+ylim([min(MarkerPos1A(2,:)) max(MarkerPos1A(2,:))])
+xlim([min(MarkerPos2A(1,:)) max(MarkerPos2A(1,:))])
+ylim([min(MarkerPos2A(2,:)) max(MarkerPos2A(2,:))])
+%
+%fill(BezierPos(1,:),BezierPos(2,:), 'k', 'EdgeColor', 'none'); 
+plot(BezierPos(1,:),BezierPos(2,:),'k','LineWidth',2)
+%
+f2 = figure('Visible','off','Name','With circle');
+
+% video object
+v = VideoWriter(strcat(VidName,".mp4"),'MPEG-4');
+v.Quality = 100;
+open(v)
+
+% main loop
+WB = waitbar(0,strcat('Generating video (',VidName,'.mp4)...'), ...
+  'Name','Spirograph over Bezier curves by Enciso-Alva (2025)');
+for i = 0:nTimes
+  % 
+  % update progressbar
+  waitbar(i/nTimes,WB);
+  if getappdata(WB,'canceling')
+    disp('Ended by user.')
+    close(v);
+    delete(WB)
+    break
+  end
+  %
+  %CurrPts = idxx(and(TimeFromMarker>=(i-1.1)/fps,TimeFromMarker<=(i+0.1)/fps));
+  CurrPts1A = idxx1A(and(TimeFromBez1A>=(i-1.1)/fps,TimeFromBez1A<=(i+0.1)/fps));
+  CurrPts2A = idxx2A(and(TimeFromBez2A>=(i-1.1)/fps,TimeFromBez2A<=(i+0.1)/fps));
+  CurrPts1B = idxx1B(and(TimeFromBez1B>=(i-1.1)/fps,TimeFromBez1B<=(i+0.1)/fps));
+  CurrPts2B = idxx2B(and(TimeFromBez2B>=(i-1.1)/fps,TimeFromBez2B<=(i+0.1)/fps));
+  if ~( isempty(CurrPts1A) & isempty(CurrPts2A) ) % if no points will be added. skip drawing loop
+  %
+  % add a few strokes of the marker, then copy to figure 2
+  set(0,"CurrentFigure",f1)
+  plot(MarkerPos1A(1,CurrPts1A),MarkerPos1A(2,CurrPts1A),'magenta')
+  plot(MarkerPos2A(1,CurrPts2A),MarkerPos2A(2,CurrPts2A),'yellow')
+  plot(MarkerPos1B(1,CurrPts1B),MarkerPos1B(2,CurrPts1B),'magenta')
+  plot(MarkerPos2B(1,CurrPts2B),MarkerPos2B(2,CurrPts2B),'yellow')
+  %
+  % add only the rotating wheel and the marker point
+  clf(f2)
+  copyobj(f1.Children,f2)
+  set(0,"CurrentFigure",f2)
+  %
+  j1A = max(CurrPts1A);
+  j2A = max(CurrPts2A);
+  j1B = max(CurrPts1B);
+  j2B = max(CurrPts2B);
+  if ~isempty(j1A)
+    RefWheelCtrA = WhCtrPos1A(:,j1A);
+    RefAngleA = MarkerAngle1A(j1A);
+  else
+    RefWheelCtrA = WhCtrPos2A(:,j2A);
+    RefAngleA = MarkerAngle2A(j2A);
+  end
+  if ~isempty(j1B)
+    RefWheelCtrB = WhCtrPos1B(:,j1B);
+    RefAngleB = MarkerAngle1B(j1B);
+  else
+    RefWheelCtrB = WhCtrPos2B(:,j2B);
+    RefAngleB = MarkerAngle2B(j2B);
+  end
+  fill(RefWheelCtrA(1)+circ(1,:),RefWheelCtrA(2)+circ(2,:), 'b', 'EdgeColor', 'none','FaceAlpha',0.5); 
+  fill(RefWheelCtrB(1)+circ(1,:),RefWheelCtrB(2)+circ(2,:), 'b', 'EdgeColor', 'none','FaceAlpha',0.5); 
+  for w = 1:size(aux_angles,2)
+    plot(RefWheelCtrA(1)+[0,cos(aux_angles(w)+RefAngleA)*WheelRadius],RefWheelCtrA(2)+[0,sin(aux_angles(w)+RefAngleA)*WheelRadius],...
+      'Color',[0,0,0,0.5])
+    plot(RefWheelCtrB(1)+[0,cos(aux_angles(w)+RefAngleB)*WheelRadius],RefWheelCtrB(2)+[0,sin(aux_angles(w)+RefAngleB)*WheelRadius],...
+      'Color',[0,0,0,0.5])
+  end
+  scatter(MarkerPos1A(1,j1A),MarkerPos1A(2,j1A),10,'magenta','filled')
+  scatter(MarkerPos2A(1,j2A),MarkerPos2A(2,j2A),10,'yellow','filled')
+  scatter(MarkerPos1B(1,j1B),MarkerPos1B(2,j1B),10,'magenta','filled')
+  scatter(MarkerPos2B(1,j2B),MarkerPos2B(2,j2B),10,'yellow','filled')
+  %
+  end
+  writeVideo(v,getframe)
+end
+
+if exist('WB','var')
+  if getappdata(WB,'canceling')
+    return
+  end
+  delete(WB)
+end
+
+% stop for some time after everything is finished
+for stopper = 0:(fps*AfterTime)
+  writeVideo(v,getframe)
+end
+
+% finalize the video object and close the figure
+close(v);
