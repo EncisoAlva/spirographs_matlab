ee5f682296c53a8c3233d004fe0f84079cd7a4ab
Julio C Enciso-Alva <juliocesar.encisoalva@mavs.uta.edu>
2026-06-05 16:56:23 -0500

    Create BezPath.m

---
+++ BezPath.m
@@ -0,0 +1,13 @@
+classdef BezPath
+  properties
+    Property1
+  end
+
+  methods
+    function obj = BezPath(inputArg1)
+      obj.Property1 = inputArg1^2;
+      disp(obj.Property1);
+      
+    end
+  end
+end
