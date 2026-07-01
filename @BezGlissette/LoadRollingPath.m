39dc4f124388b0036bedafb238009de2114ee532
Julio C Enciso-Alva <juliocesar.encisoalva@mavs.uta.edu>
2026-07-01 14:54:06 -0500

    Update LoadRollingPath.m

--- @BezGlissette/LoadRollingPath.m
+++ @BezGlissette/LoadRollingPath.m
@@ -1,5 +1,5 @@
 function obj = LoadRollingPath( obj, varargin )
   obj.OG_BPath = BezPath( varargin );
-  obj.OG_BPath = obj.OG_BPath.Shift( obj.Shiften, obj.Halfen );
+  %obj.OG_BPath = obj.OG_BPath.Shift( obj.Shiften, obj.Halfen );
   obj.BPath = obj.OG_BPath;
 end
